e0d02619019c9cacbaded31bbb2f669c8e84556d
santyac <santyac27@hotmail.com>
2016-10-03 19:10:28 +0200

    filosof correcte

--- hello_world/hello_world.java
+++ hello_world/hello_world.java
@@ -1,1 +1,1 @@
-alert("Hello world: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
+alert("Hello world: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
